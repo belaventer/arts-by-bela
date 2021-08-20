965697a49ad3121c80832d32b818c03e0f2198c4
belaventer <isaventer1@hotmail.com>
2021-08-20 19:31:03 +0000

    Update Resolution and Size models, start price calculator

--- commissions/models.py
+++ commissions/models.py
@@ -2,51 +2,43 @@ from django.db import models
 
 
 class Resolution(models.Model):
-    LOW = 72
-    MEDIUM = 144
-    HIGH = 300
-    RESOLUTIONS_CHOICES = [
-        (LOW, '72 dpi'),
-        (MEDIUM, '144 dpi'),
-        (HIGH, '300 dpi'),
-    ]
-    resolution = models.IntegerField(
-        choices=RESOLUTIONS_CHOICES,
-        default=HIGH,
-        unique=True
+
+    class ResolutionsChoices(models.TextChoices):
+        LOW = '72 dpi'
+        MEDIUM = '144 dpi'
+        HIGH = '300 dpi'
+
+    resolution = models.CharField(
+        choices=ResolutionsChoices.choices,
+        default=ResolutionsChoices.LOW,
+        unique=True,
+        max_length=7,
     )
 
     price_factor = models.DecimalField(
         default=1, max_digits=4, decimal_places=2)
 
-    def __str__(self, RESOLUTIONS_CHOICES=RESOLUTIONS_CHOICES):
-        for choice in RESOLUTIONS_CHOICES:
-            if self.resolution == choice[0]:
-                return choice[1]
+    def __str__(self):
+        return self.resolution
 
 
 class Size(models.Model):
-    A4 = '210 x 297 mm'
-    A5 = '148 x 210 mm'
-    A6 = '105 x 148 mm'
-    A7 = '74 x 105 mm'
-    SIZES_CHOICES = [
-        (A4, 'A4'),
-        (A5, 'A5'),
-        (A6, 'A6'),
-        (A7, 'A7'),
-    ]
+
+    class SizesChoices(models.TextChoices):
+        A4 = 'A4 210 x 297 mm'
+        A5 = 'A5 148 x 210 mm'
+        A6 = 'A6 105 x 148 mm'
+        A7 = 'A7 74 x 105 mm'
+
     size = models.CharField(
-        choices=SIZES_CHOICES,
-        default=A6,
+        choices=SizesChoices.choices,
+        default=SizesChoices.A7,
         unique=True,
-        max_length=12,
+        max_length=15,
     )
 
     price_factor = models.DecimalField(
         default=1, max_digits=4, decimal_places=2)
 
-    def __str__(self, SIZES_CHOICES=SIZES_CHOICES):
-        for choice in SIZES_CHOICES:
-            if self.size == choice[0]:
-                return choice[1]
+    def __str__(self):
+        return self.size
--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from commissions import models
 
 
 def index(request):
     """ A view to return the home page """
+    resolutions = models.Resolution.objects.all()
+    sizes = models.Size.objects.all()
+    context = {
+        'resolutions': resolutions,
+        'resolutions_json': list(resolutions.values()),
+        'sizes': sizes,
+        'sizes_json': list(sizes.values()),
+    }
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
