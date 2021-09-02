875aaecd41ab3637ea9b23e76d995dbb947577e4
belaventer <isaventer1@hotmail.com>
2021-09-02 09:12:27 +0000

    Add profile form and post action

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
+@login_required
 def profile(request):
     """ A view to return the profile page """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=profile)
+
     context = {
         'profile': profile,
+        'form': form,
     }
 
     return render(request, 'profiles/profile.html', context)
